4dd18db6a29eb3b5fe66359567f24e26225c362b
michael93pl <michael93pl@gmail.com>
2017-12-05 22:16:32 +0100

    added displaying function

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from sqlalchemy import Column, Integer, Numeric, String, Enum, ForeignKey
 import enum
 import somestuff
 
+#Preparation of MYSQL Alchemy and creating tables
 my_database = 'mysql+mysqldb://root:' + somestuff.mysql_pass + '@localhost/grocerylist'
-engine = create_engine(my_database, echo=True)
+engine = create_engine(my_database)
 
 connection = engine.connect()
 
 Base = declarative_base()
 session = sessionmaker(bind=engine)
-
 
 class MyEnum(enum.Enum):
     szt = "szt"
@@ -38,67 +38,44 @@ class Product(Base):
 Base.metadata.create_all(engine)
 
 
+print("Hello dear User!\nYou can either add new items to your grocery list or check your list\nWhat would you like to do? Please choose 'add' or 'check' only.")
 
 
+#category template with the category printing function
+category_msg_template = ["1. Vegetables/Fruits", "2. Dairy products", "3. Meat", "4. Chemistry",
+                        "5. Cosmetics", "6. Bread", "7. Drinks", "8. Alkohol", "9. Snacks", "10. Others"]
 
-print("Hello dear User!\nYou can either add new items to your grocery list or check your list\nWhat would you like to do? Please choose add or check only.")
 
-def template():
-    msg_template = """
-    
-                """
-    pass
-
+def category_printing():
+    for i in category_msg_template:
+        print(i)
 
 def first_choise():
-    """checking input"""
+    """checking if the user wants to add the new item or view the whole list"""
     while True:
         choice = input().strip().lower()
         if choice == "add" or choice == "a":
-            print(funkcja_do_dodawania(10,15))
-            break
+            return display()
         elif choice == "check" or choice == "c":
             print(funkcja_do_wyswietlania(300,500))
             break
         else:
-            print("You messed up. Please choose add new item or check the current list only.")
+            print("You messed up. Please choose to add a new item or check the current list only.")
 
+def display():
+    """Inserting data into database"""
+    print("At first pick one of the following categories:\n")
+    return category_printing()
 
-def funkcja_do_dodawania(kupa, siku):
-    kibelek = kupa + siku
-
-    return kibelek
-
+    while True:
+        category_choise = input().strip().lower()
+        if category_choise == "1" or category_choise == "vegetables":
+            print("Twoja stara")
 
 def funkcja_do_wyswietlania(cipka, kutas):
     lista = cipka + kutas
 
     return lista
 
-
-
-
-
-
-
-
 if __name__ == "__main__":
     first_choise()
-
-
-
-#Grocery list app with MySQL
-
-"""czy chcesz dodac // czy chcesz wczytac
-
-wyswietlac cala baze, same kategorie (oby dwie z description)
-
-
-
-!!!znaleźć operacje na metodach, zeby przetestować klasy !!!
-
-
-dodawania / odejmowanie / usuwanie itemow
-
-
-"""
